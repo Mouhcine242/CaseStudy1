f191cf1f3c2d6395fa12ab8ea6b9893e4540d3dc
Mouhcine242 <81862827+Mouhcine242@users.noreply.github.com>
2021-04-07 00:46:44 +0100

    Add files via upload

--- Desktop.java
+++ Desktop.java
@@ -1,5 +1,28 @@
 package Classe;
 
+import java.util.*;
 public class Desktop {
+	
+	static TreeSet<Raccourcis> ensR = new TreeSet<>();
+	
+    public void creer_raccourcif(Fichier f) {
+		
+		Raccourcis R=new Raccourcis(f.nom,f.taille,f.path);
+		Desktop.ensR.add(R);
+		
+	}
+    public void creer_raccourciD(Dossier d) {
+		
+		Raccourcis R=new Raccourcis(d.nom,d.taill,d.path);
+		Desktop.ensR.add(R);
+		
+	}
+    public void creer_raccourciR(Repertoire r) {
+		
+		Raccourcis R=new Raccourcis(r.nom,r.taill,r.path);
+		Desktop.ensR.add(R);
+		
+	}
 
+	
 }
--- Raccourcis.java
+++ Raccourcis.java
@@ -1,5 +1,43 @@
 package Classe;
 
-public class Raccourcis {
+import java.util.*;
+public class Raccourcis implements Element {
+	String nom ;
+	double taille ;
+	String path ;
+	Scanner cl = new Scanner(System.in);
+	Raccourcis(String nom , double taille, String path){
+		this.nom=nom;
+		this.path=path;
+		this.taille=taille ;
+	}
+	public void Supp(Raccourcis r) {
+		System.out.println("Enter your UserName");
+		String s1 = cl.next();
+		System.out.println("Enter your Password");
+		String s2 = cl.next();
+		if(s1.equals(User.auth1())&& s2.equals(User.auth2()) && Desktop.ensR.contains(r)) {
+			Dossier.ensF.remove(r);
+		}
+		else {
+			System.out.println("Error");
+		}
+		
+	}
+	@Override
+	public String getName() {
+		return this.nom;
+	}
+	@Override
+	public double getLenght() {
+		return this.taille;
+	}
+	@Override
+	public void New(String nom, double taill) {
+
+		
+	}
+
+	
 
 }
